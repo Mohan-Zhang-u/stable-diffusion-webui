b7d5b1ff7bdb870cf796119d00b535b056e0de11
Mohan Zhang <mohan.zhang.mz@gmail.com>
2023-06-13 22:19:51 -0400

    added more tips

--- tips.sh
+++ tips.sh
@@ -1,2 +1,11 @@
 # run with
 python launch.py
+
+url in pyproject.toml, http://127.0.0.1:7860
+
+# upscaling
+https://www.youtube.com/watch?v=9EACJiy5AMU
+
+
+https://civitai.com/models/72675/breakdomainanime
+https://civitai.com/models/35960/flat-2d-animerge
